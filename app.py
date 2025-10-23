52551b53ba9fa85c1fe76d90058f9f3454e1d7a1
Ananda Gopal Reddy Manne <reddyanandagopal@gmail.com>
2025-10-23 20:32:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning)
+warnings.filterwarnings("ignore", category=FutureWarning)
+
 import streamlit as st
 import traceback
 import pathlib
